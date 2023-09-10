86e22b0f2ba5f1e804002ecf4be93971e77e4555
Joko Nugroho <jocoding@gmail.com>
2023-09-10 12:21:27 +0700

    ready

--- src/script.js
+++ src/script.js
@@ -1,12 +1,19 @@
 const message = 'Hello world' // Try edit me
 
-elemenRoot.innerHTML = message
-const elButtonXls = elemenButton
-elButtonXls.id = "download-xlsx"
-elButtonXls.innerText = 'Buat Xlsx'
-elemenRoot.appendChild(elButtonXls)
+function initAll() {
+  elemenRoot.innerHTML = message
+  const elButtonXls = elemenButton
+  elButtonXls.id = "download-xlsx"
+  elButtonXls.innerText = 'Buat Xlsx'
+  elemenRoot.appendChild(elButtonXls)
 
-document.getElementById("download-xlsx").addEventListener("click", genXlsx)
+  document.getElementById("download-xlsx").addEventListener("click", genXlsx)
 
-// Log to console
-console.log(message)
+  // Log to console
+  console.log(message)
+
+}
+
+$(document).ready(function () {
+  initAll()
+});
